2a1df44455c52ea5d5c3f722dca374fc1aa66e15
ewaugh <ewaugh@lab1-23.eng.utah.edu>
2018-01-18 20:42:05 -0700

    work with kevan and jacob

--- PA1/1A/TCPServer.py
+++ PA1/1A/TCPServer.py
@@ -1,27 +1,67 @@
 from socket import *
 from urllib.parse import urlparse
 
-server_address = ('localhost', 2101)
+server_address = ('localhost', 2117)
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind(server_address)
 server_socket.listen(5)
 print('The server is ready to receive')
 while True:
     connection_socket, addr = server_socket.accept()
-    sentence = connection_socket.recv(1024).decode()
-    #if we have a full request do this
-    sentence_split = sentence.split()
 
-    parsed_sentence = urlparse(sentence)
-
-    #else figure out how to handle multiple line requests
-    request_socket = socket(AF_INET, SOCK_STREAM)
     request_port = 80
-    if parsed_sentence.port != None:
-        request_port = parsed_sentence.port
+    method = http_version = netloc = url_path = None
+    first_recv = request_status_ok = True
+    while method is None or http_version is None or netloc is None or url_path is None:
+        sentence = connection_socket.recv(4096).decode()
+        if sentence == "\r\n":
+            print("breaking")
+            break
+        if first_recv:
+            sentence_split = sentence.split()
+            if len(sentence_split) != 3:
+                error_message = "not a proper http request"
+                connection_socket.send(error_message.encode())
+                connection_socket.close()
+                request_status_ok = False
+                break
 
-    request_socket.connect((parsed_sentence.netloc, request_port))
-    get_request = "GET " + parsed_sentence.path + " HTTP/1.0"
+            method = sentence_split[0]
+            http_version = sentence_split[2]
+            first_recv = False
+            parsed_sentence = urlparse(sentence_split[1])
+
+            if parsed_sentence.port != "" and parsed_sentence.port is not None:
+                request_port = parsed_sentence.port
+            if parsed_sentence.netloc != "" and parsed_sentence.netloc is not None:
+                netloc = parsed_sentence.netloc
+            if parsed_sentence.path != "" and parsed_sentence.path is not None:
+                url_path = parsed_sentence.path
+        else:
+            sentence_split = sentence.split()
+            #check for 2 words and that it is one of the header tags
+            if sentence_split[0] == "Host:":
+                netloc = sentence_split[1]
+
+    if method != "GET":
+        error_message = "not a proper http request"
+        connection_socket.send(error_message.encode())
+        connection_socket.close()
+        request_status_ok = False
+    if http_version != "HTTP/1.0"
+        error_message = "not a proper http request"
+        connection_socket.send(error_message.encode())
+        connection_socket.close()
+        request_status_ok = False
+
+    if request_status_ok == False:
+        continue
+
+    #check method, then http version, then host and then endpoint
+    request_socket = socket(AF_INET, SOCK_STREAM)
+
+    request_socket.connect((netloc, request_port))
+    get_request = method + " " + url_path + " " + http_version + "\r\n" + "Host: " + netloc + "\r\n" + "Connection: close" + "\r\n\r\n"
     request_socket.sendall(get_request.encode())
 
     html_response = request_socket.recv(4096)
@@ -30,8 +70,4 @@ while True:
     request_socket.close()
     connection_socket.close()
 
-
-#questions for TAs
-#1 how to handle seperate messages from telnet
-#2 how to seperate get request line
-#3 how to validate its a proper request line
+#ask how to pass in port
